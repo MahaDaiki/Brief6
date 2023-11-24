193426693ed1c3b0b06a99fbaab3980e1f8c36a6
MahaDaiki <mahadaiki2@gmail.com>
2023-11-24 15:27:29 +0100

    navedit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
 let subMenu = document.getElementById("subMenu");
-function toggleMenu(){
+// function toggleMenu(){
+//     subMenu.classList.toggle("menuwrpopen");
+// }
+
+let hj = document.querySelector('.user-pic');
+hj.addEventListener('click', function(){
+    console.log("clicked")
     subMenu.classList.toggle("menuwrpopen");
-}
+})
