7b369c7b7f84d9793bb380232d4e17f9ceae469b
MahaDaiki <mahadaiki2@gmail.com>
2023-11-27 12:01:01 +0100

    php

--- register.php
+++ register.php
@@ -18,5 +18,38 @@
         <button class=" bttn btn btn-primary " type="submit">Sign Up</button>
         <a href="index.html">Log In</a>
   </form>
+  <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $email = $_POST["email"];
+    $password = password_hash($_POST["password"], PASSWORD_BCRYPT);
+
+    // Establish a database connection (replace these with your actual database details)
+    $servername = "localhost";
+    $username = "root";
+    $password = "maha123";
+    $dbname = "electronacerdb2";
+
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+   
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+  
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+
+    if (mysqli_query($conn, $sql)) {
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+}
+?>
+
 </body>
 </html>
